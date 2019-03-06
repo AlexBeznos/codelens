cb305d1eccf4bc78074a1a8bf7925ac356dd6a0a
dmitrykutran <dkutran@jssolutions.com>
2019-03-06 19:45:44 +0200

    Change editor framework

--- src/layouts/EditPane/index.js
+++ src/layouts/EditPane/index.js
@@ -1,32 +1,54 @@
-import React, { useState, useRef } from 'react';
-import useThrottle from '../../hooks/useThrottle';
-import { Editor } from 'slate-react';
-import { Value } from 'slate';
-import data from './data';
+import React, { useState } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/theme/solarized.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/ruby/ruby';
+import { Controlled as CodeMirror } from 'react-codemirror2';
+
+const THEMES = ['monokai', 'solarized'];
+const MODES = ['javascript', 'ruby'];
+
+const options = {
+  indentUnit: 2,
+  tabSize: 2,
+  indentWithTabs: false,
+};
 
 function EditPane() {
-  const editorRef = useRef();
-  const [value, setValue] = useState(() => Value.fromJSON(data));
-  const pushOperation = useThrottle(operations => {
-    console.log('send it', operations);
-  }, 500);
+  const [value, setValue] = useState('function() {}');
+  const [mode, setMode] = useState(MODES[0]);
+  const [theme, setTheme] = useState(THEMES[0]);
 
-  function onChange(change) {
-    change.operations
-      .filter(e => e.get('type') !== 'set_selection')
-      .toJS()
-      .forEach(pushOperation);
+  function onBeforeChange(editor, data, value) {
+    setValue(value);
+  }
 
-    setValue(change.value);
+  function onModeChange(e) {
+    setMode(e.target.value);
+  }
+
+  function onThemeChange(e) {
+    setTheme(e.target.value);
   }
 
   return (
-    <Editor
-      placeholder="Enter some text..."
-      ref={editorRef}
-      value={value}
-      onChange={onChange}
-    />
+    <>
+      <select value={mode} onChange={onModeChange}>
+        <option value="javascript">javascript</option>
+        <option value="ruby">ruby</option>
+      </select>
+      <select value={theme} onChange={onThemeChange}>
+        <option value="solarized">solarized</option>
+        <option value="monokai">monokai</option>
+      </select>
+      <CodeMirror
+        value={value}
+        options={{ ...options, mode, theme }}
+        onBeforeChange={onBeforeChange}
+      />
+    </>
   );
 }
 
